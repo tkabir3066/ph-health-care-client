6bacd9da6fec8316c71a262be4fef57d75e2400f
Tutul <tutulkabir@gmail.com>
2026-02-14 19:42:26 +0530

    Implement routing architecture and authentication forms with validation and server actions

---
+++ src/components/RegisterForm.tsx
@@ -0,0 +1,107 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use client";
+import { useActionState } from "react";
+import { Button } from "./ui/button";
+import { Field, FieldDescription, FieldGroup, FieldLabel } from "./ui/field";
+import { Input } from "./ui/input";
+import { registerPatient } from "@/services/auth/registerPatient";
+
+const RegisterForm = () => {
+  const [state, fornAction, isPending] = useActionState(registerPatient, null);
+  const getFieldError = (fieldName: string) => {
+    if (state && state.errors) {
+      const error = state.errors.find((err: any) => err.field === fieldName);
+      return error?.message;
+    } else {
+      return null;
+    }
+  };
+  return (
+    <form action={fornAction}>
+      <FieldGroup>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          {/* Name */}
+          <Field>
+            <FieldLabel htmlFor="name">Full Name</FieldLabel>
+            <Input id="name" name="name" type="text" placeholder="John Doe" />
+            {getFieldError("name") && (
+              <FieldDescription className="text-red-600">
+                {getFieldError("name")}
+              </FieldDescription>
+            )}
+          </Field>
+          {/* Address */}
+          <Field>
+            <FieldLabel htmlFor="address">Address</FieldLabel>
+            <Input
+              id="address"
+              name="address"
+              type="text"
+              placeholder="123 Main St"
+            />
+            {getFieldError("address") && (
+              <FieldDescription className="text-red-600">
+                {getFieldError("address")}
+              </FieldDescription>
+            )}
+          </Field>
+          {/* Email */}
+          <Field>
+            <FieldLabel htmlFor="email">Email</FieldLabel>
+            <Input
+              id="email"
+              name="email"
+              type="email"
+              placeholder="m@example.com"
+            />
+            {getFieldError("email") && (
+              <FieldDescription className="text-red-600">
+                {getFieldError("email")}
+              </FieldDescription>
+            )}
+          </Field>
+          {/* Password */}
+          <Field>
+            <FieldLabel htmlFor="password">Password</FieldLabel>
+            <Input id="password" name="password" type="password" />
+            {getFieldError("password") && (
+              <FieldDescription className="text-red-600">
+                {getFieldError("password")}
+              </FieldDescription>
+            )}
+          </Field>
+          {/* Confirm Password */}
+          <Field className="md:col-span-2">
+            <FieldLabel htmlFor="confirmPassword">Confirm Password</FieldLabel>
+            <Input
+              id="confirmPassword"
+              name="confirmPassword"
+              type="password"
+            />
+            {getFieldError("confirmPassword") && (
+              <FieldDescription className="text-red-600">
+                {getFieldError("confirmPassword")}
+              </FieldDescription>
+            )}
+          </Field>
+        </div>
+        <FieldGroup className="mt-4">
+          <Field>
+            <Button type="submit" disabled={isPending}>
+              {isPending ? "Creating Account" : "Creater Account"}
+            </Button>
+
+            <FieldDescription className="px-6 text-center">
+              Already have an account?{" "}
+              <a href="/login" className="text-blue-600 hover:underline">
+                Sign in
+              </a>
+            </FieldDescription>
+          </Field>
+        </FieldGroup>
+      </FieldGroup>
+    </form>
+  );
+};
+
+export default RegisterForm;
